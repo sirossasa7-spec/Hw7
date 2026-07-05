0ac5324898c07285e6cb4648be61ad46839b9f92
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-05 16:01:55 +0200

    Add project files.

---
+++ Hw-7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Hw_7
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    class Array : IOutput
+    {
+        private int[] numbers;
+
+        public Array(int[] arr)
+        {
+            numbers = arr;
+        }
+
+        public void Show()
+        {
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array arr = new Array(new int[] { 5, 8, 2, 9, 1 });
+
+            arr.Show();
+
+            arr.Show("Мій масив:");
+
+            Console.ReadKey();
+        }
+    }
+}
