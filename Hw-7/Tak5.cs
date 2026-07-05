474df5c54fc77062f9839cd063d42d8bd6ca2224
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-07-05 16:36:27 +0200

    Task5

---
+++ Hw-7/Tak5.cs
@@ -0,0 +1,69 @@
+﻿namespace Hw_7
+{
+    internal class Tak5
+    {
+        interface IDrivable
+        {
+            void StartEngine();
+            void StopEngine();
+            void Drive();
+        }
+
+        class Car : IDrivable
+        {
+            public void StartEngine()
+            {
+                Console.WriteLine("Автомобіль: двигун запущено.");
+            }
+
+            public void StopEngine()
+            {
+                Console.WriteLine("Автомобіль: двигун вимкнено.");
+            }
+
+            public void Drive()
+            {
+                Console.WriteLine("Автомобіль їде.");
+            }
+        }
+
+        class Motorcycle : IDrivable
+        {
+            public void StartEngine()
+            {
+                Console.WriteLine("Мотоцикл: двигун запущено.");
+            }
+
+            public void StopEngine()
+            {
+                Console.WriteLine("Мотоцикл: двигун вимкнено.");
+            }
+
+            public void Drive()
+            {
+                Console.WriteLine("Мотоцикл їде.");
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                IDrivable car = new Car();
+                IDrivable bike = new Motorcycle();
+
+                Console.WriteLine("=== Автомобіль ===");
+                car.StartEngine();
+                car.Drive();
+                car.StopEngine();
+
+                Console.WriteLine();
+
+                Console.WriteLine("=== Мотоцикл ===");
+                bike.StartEngine();
+                bike.Drive();
+                bike.StopEngine();
+            }
+        }
+    }
+}
